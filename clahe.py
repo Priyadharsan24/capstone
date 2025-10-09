818396a736bad8e76bd3754b9ee9854138a12c1b
Yadhu <kpypro2005@gmail.com>
2025-10-09 20:42:08 +0530

    Tried CLAHE on thermal image

--- clahe.py
+++ clahe.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('thermal_image.jpg', cv2.IMREAD_GRAYSCALE)  # read image
+img = cv2.imread(r'../thermal_image.jpg', cv2.IMREAD_GRAYSCALE)  # read image
 
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))  # create clahe
 enhanced = clahe.apply(img)  # apply clahe
@@ -11,4 +11,4 @@ cv2.imshow('Enhanced', enhanced)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imwrite('thermal_clahe.jpg', enhanced)  # save result
+cv2.imwrite(r'../thermal_clahe.jpg', enhanced)  # save result
